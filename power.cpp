2e5f6c111caef70174c111812ff1967bf6909200
Xuefer <xuefer@gmail.com>
2016-01-11 21:58:34 +0800

    power: update power version
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- power.cpp
+++ power.cpp
@@ -144,7 +144,7 @@ static struct hw_module_methods_t power_module_methods = {
 struct power_module HAL_MODULE_INFO_SYM = {
     common: {
         tag: HARDWARE_MODULE_TAG,
-        module_api_version: POWER_MODULE_API_VERSION_0_2,
+        module_api_version: POWER_MODULE_API_VERSION_0_3,
         hal_api_version: HARDWARE_HAL_API_VERSION,
         id: POWER_HARDWARE_MODULE_ID,
         name: "Shield Power HAL",
